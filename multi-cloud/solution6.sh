41b2215f983542af5a9a44e08911f5679d7d4b98
Holden Karau <holden@pigscanfly.ca>
2019-05-07 12:12:08 -0700

    Revert "move solutions"
    This reverts commit dcd27d036f28e57cc04e0586886b44270ad07382.

--- multi-cloud/solution6.sh
+++ multi-cloud/solution6.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-cd ~/example-seldon/workflows
-argo submit serving-sk-mnist-workflow.yaml -n kubeflow -p deploy-model=true
+argo submit training-tf-mnist-workflow.yaml -n kubeflow 
